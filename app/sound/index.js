5266bd6f7ad244aba8ed08b137ede0d7bf5f9790
Marc Nitzsche <marc.nitzsche@live.de>
2018-03-13 21:29:00 +0100

    Improve selection of note names

--- app/sound/index.js
+++ app/sound/index.js
@@ -3,13 +3,12 @@ import { Synth } from 'tone';
 const synth = new Synth().toMaster();
 
 const scale = [
-    'C2', 'D2', 'Eb2', 'F2', 'G2', 'Ab2', 'A2', 'B2',
-    'C3', 'D3', 'Eb3', 'F3', 'G3', 'Ab3', 'A3', 'B3',
+    'C3', 'D3', 'Eb3', 'G3', 'Ab3', 'A3', // almost minor pentatonic, but add the Ab for
+    'C4', 'D4', 'Eb4', 'G4', 'Ab4', 'A4', // some tension
 ];
 
 const MAX_NOTE_HEIGHT = 100;
-window.synth = synth;
-console.log(scale);
+
 export function playNote(noteHeight) {
     const note = Math.floor(scale.length / MAX_NOTE_HEIGHT * noteHeight);
     synth.triggerAttackRelease(scale[note], '16n')
