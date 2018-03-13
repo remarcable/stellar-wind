46a3c5ea6d9fd567b6120d8de9fafabf31127871
Marc Nitzsche <marc.nitzsche@live.de>
2018-03-13 21:22:52 +0100

    Add random values between 1 and 100 to simulate value of transactions

--- app/sound/index.js
+++ app/sound/index.js
@@ -2,8 +2,17 @@ import { Synth } from 'tone';
 
 const synth = new Synth().toMaster();
 
-export function playNote() {
-    synth.triggerAttackRelease('C4', '16n')
+const scale = [
+    'C2', 'D2', 'Eb2', 'F2', 'G2', 'Ab2', 'A2', 'B2',
+    'C3', 'D3', 'Eb3', 'F3', 'G3', 'Ab3', 'A3', 'B3',
+];
+
+const MAX_NOTE_HEIGHT = 100;
+window.synth = synth;
+console.log(scale);
+export function playNote(noteHeight) {
+    const note = Math.floor(scale.length / MAX_NOTE_HEIGHT * noteHeight);
+    synth.triggerAttackRelease(scale[note], '16n')
 }
 
 export function playBackgroundSounds() {
