8d15a4d09cdc70b4a439a83ece5b8c1da9be8cc9
Christian Haug <csh@christianhaug.com>
2018-03-14 00:29:22 +0100

    Add calculation of normalized operation amount

--- app/app.js
+++ app/app.js
@@ -9,12 +9,18 @@ playBackgroundSounds();
 
 let transactions = [];
 let operationAmounts = [];
+let maxOperationAmount = 0;
+
+const normalizationScale = 100;
 
 createNewStreamOfTransactions((response) => {
     transactions.push(response);
     const txOperationAmounts = getNonZeroAmountsFromOperations(response);
     txOperationAmounts.forEach(amount => {
         operationAmounts.push(amount);
+        if (amount > maxOperationAmount) {
+            maxOperationAmount = amount;
+        }
     });
 });
 
@@ -23,6 +29,8 @@ window.setInterval(() => {
 
     if (shouldUpdate && operationAmounts.length > 0) {
         const currentAmount = operationAmounts.pop();
+        const normalizedAmount = Math.ceil((currentAmount / maxOperationAmount) * normalizationScale);
+        console.log('normalizedAmount:', normalizedAmount);
         shootNewStar();
         playNote();
     }
