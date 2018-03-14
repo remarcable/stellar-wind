6269c7a599d0f9d7a31ac64a25b70087ed3e29b2
Marc Nitzsche <marc.nitzsche@live.de>
2018-03-14 23:40:49 +0100

    Improve lead sound

--- app/sound/lead.js
+++ app/sound/lead.js
@@ -1,3 +1,31 @@
-import { Synth } from 'tone';
+import { Synth, Chorus, PingPongDelay, PitchShift } from 'tone';
 
-export default new Synth().toMaster();
+
+const delay = new PingPongDelay({
+    delayTime: .2,
+    maxDelayTime: .3,
+}).toMaster();
+
+const pitchShift = new PitchShift({
+    pitch: 8,
+    wet: .05,
+}).connect(delay);
+
+const chorus = new Chorus({
+    delayTime: 2,
+    wet: .4,
+})
+.connect(pitchShift)
+.toMaster();
+
+export default new Synth({
+    oscillator: {
+        type: 'sine',
+    },
+    envelope: {
+        attack: .1,
+        sustain: .9,
+        release: 1,
+    },
+    volume: -12,
+}).connect(chorus);
